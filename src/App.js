4da76fdd4565d1f0ddf72f9a54240131b3d19368
anusha211 <34378028+anusha211@users.noreply.github.com>
2018-05-20 00:12:35 +0545

    .

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
     return (
       <div className="App">
      <Sidebar/>
-     </div>
+  
+    </div>
     )
   }
 }
--- src/containers/Sidebar.js
+++ src/containers/Sidebar.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import './Sidebar.css';
 
+
 const Sidebar=() => (
 
     <div className="Sidebar">
       <nav>
         <ul>
-          <li>  <button><a href='/'> Account</a> </button></li>
-          <li>  <a href='/'> Home</a></li>
-          <li><a href='/'> Calendar</a></li>
-          <li><a href='/'> Logout</a></li>
+           <button> <li> <a href='/'> Account</a></li> </button>
+           <button> <li> <a href='/'> Home</a></li>  </button>
+           <button> <li> <a href='/'> Routine</a></li> </button> 
+           <button> <li> <a href='/'> Notice</a></li> </button> 
+            <body/>
+           <button> <li> <a href='/'>  Logout</a> </li> </button> 
+          
         </ul>
       </nav>
       
